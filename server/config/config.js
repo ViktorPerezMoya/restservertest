df8301b4b82a9a0db65895e3447ff85a64ef71ad
Victor Ariel Perez <victor.ariel.perez@gmail.com>
2020-09-11 18:08:55 -0300

    test

--- server/config/config.js
+++ server/config/config.js
@@ -5,4 +5,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if(process.env.NODE_ENV === 'dev')
     process.env.URL_DB =  'mongodb://localhost:27017/cafe';
 else 
-    process.env.URL_DB = process.env.MONGO_URI;
+    process.env.URL_DB = process.env.MONGO_URI; 
