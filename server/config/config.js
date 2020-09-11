d18c9b66d4c6435f2ae32a05885994d0efaf1f08
Victor Ariel Perez <victor.ariel.perez@gmail.com>
2020-09-11 18:04:06 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -5,9 +5,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if(process.env.NODE_ENV === 'dev')
     process.env.URL_DB =  'mongodb://localhost:27017/cafe';
 else 
-    process.env.URL_DB = 'mongodb+srv://root:gwFqZB2XCVVvrpmn@cluster0.sdff1.mongodb.net/cafe';
-
-//USUARIO MONGO ATLAS
-//user root
-//pass gwFqZB2XCVVvrpmn
-//URL: mongodb+srv://root:<password>@cluster0.sdff1.mongodb.net/test
+    process.env.URL_DB = process.env.MONGO_URI;
